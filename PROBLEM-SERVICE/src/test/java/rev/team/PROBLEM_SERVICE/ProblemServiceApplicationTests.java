7415161dee676c245008dd79814c23df1d954b12
xodud001 <freeptk1@gmail.com>
2021-05-14 17:52:19 +0900

    problem/21-05-14/ 시험 결과 보기 기능 리팩토링

---
+++ PROBLEM-SERVICE/src/test/java/rev/team/PROBLEM_SERVICE/ProblemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rev.team.PROBLEM_SERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
