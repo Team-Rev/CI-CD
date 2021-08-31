38f0f96597c1f7469334fff394d349f1a58a2f8b
EunsilSon <eunsil1023@gmail.com>
2021-08-31 15:55:42 +0900

    auth/21-08-31

--- AUTH-SERVER/src/main/java/rev/team/AUTHSERVER/config/SecurityConfig.java
+++ AUTH-SERVER/src/main/java/rev/team/AUTHSERVER/config/SecurityConfig.java
@@ -10,6 +10,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("findId").permitAll();
+                .authorizeRequests().antMatchers("**").permitAll();
     }
 }
