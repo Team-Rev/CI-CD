78eaa1bb1c80007ae1ef583248f8ea1c366ed2a1
EunsilSon <eunsil1023@gmail.com>
2021-09-02 11:51:37 +0900

    auth/21-09-02/changeNewPw 컨트롤러 수정

--- AUTH-SERVER/src/main/java/rev/team/AUTHSERVER/repository/RevUserRepository.java
+++ AUTH-SERVER/src/main/java/rev/team/AUTHSERVER/repository/RevUserRepository.java
@@ -18,7 +18,7 @@ public interface RevUserRepository extends JpaRepository<RevUser, String> {
     @Modifying
     @Transactional
     @Query(value = "UPDATE RevUser u SET u.password = :newPassword WHERE u.userId = :userId")
-    int updatePwByUserId(String userId, String newPassword);
+    boolean updatePwByUserId(String userId, String newPassword);
 
 
 }
--- AUTH-SERVER/src/main/java/rev/team/AUTHSERVER/service/UserService.java
+++ AUTH-SERVER/src/main/java/rev/team/AUTHSERVER/service/UserService.java
@@ -75,7 +75,7 @@ public class UserService {
     }
 
     public String changeNewPw(NewPwReq newPwReq) {
-        if(userRepository.updatePwByUserId(newPwReq.getUserId(), newPwReq.getNewPassword()) == 1) {
+        if(userRepository.updatePwByUserId(newPwReq.getUserId(), newPwReq.getNewPassword())) {
             return "SUCCESS";
         } else {
             return "FAIL";
