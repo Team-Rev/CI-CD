0e03743aedd4eed06e272b98187062df1b5b9b12
EunsilSon <eunsil1023@gmail.com>
2021-09-15 11:53:07 +0900

    api-gateway/21-09-15/로그인 실패 시 수정

--- API-GATEWAY/src/main/java/rev/team/API_GATEWAY/contorller/AuthenticationController.java
+++ API-GATEWAY/src/main/java/rev/team/API_GATEWAY/contorller/AuthenticationController.java
@@ -33,12 +33,11 @@ public class AuthenticationController {
 
     @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)throws Exception{
-        RestTemplate api = new RestTemplate();
         try{
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
             );
-        }catch (BadCredentialsException e){
+        }catch (Exception e){
             throw new Exception("Incorrect username or password", e);
         }
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
