b65e6bc622253c12ded07002e7b95a70db39e49a
EunsilSon <eunsil1023@gmail.com>
2021-09-15 17:42:39 +0900

    board/21-09-15/comment 작성 수정 및 테이블 재 생성

--- BOARD-SERVICE/src/main/java/rev/team/BOARD_SERVICE/domain/repository/CommentRepository.java
+++ BOARD-SERVICE/src/main/java/rev/team/BOARD_SERVICE/domain/repository/CommentRepository.java
@@ -1,6 +1,5 @@
 package rev.team.BOARD_SERVICE.domain.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
